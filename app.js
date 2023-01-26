a8a27a7acb04568decc05da60363fecea1197922
heisid <weird_alien@protonmail.com>
2023-01-26 19:48:16 +0700

    looks way better

--- app.js
+++ app.js
@@ -1,20 +1,24 @@
 let timer;
-let target = null;
+let target = "0:2:10";
+
+document.addEventListener("DOMContentLoaded", function(event) {
+    document.getElementById('display').innerHTML = target;
+});
 
 function start() {
     document.getElementById('startBtn').disabled = true;
-    let remaining = target ?? "0:2:10";
+    let remaining = target ?? "0:5:10";
 
     timer = setInterval(function() {
         document.getElementById('display').innerHTML = remaining;
-        if (remaining === '0:0:0') {
-            stop();
-        }
 
         const remainingArr = remaining.split(':');
         let hour = parseInt(remainingArr[0]);
         let minute = parseInt(remainingArr[1]);
         let second = parseInt(remainingArr[2]);
+        if (hour === 0 && minute === 0 && second === 0) {
+            clearInterval(timer);
+        }
 
         if (second === 0) {
             second = 59;
